f90ae7922fafc255705b790efe58e5cae0f686da
PavloShchur <pavloshchur@gmail.com>
2019-01-14 22:39:53 +0200

    Connection established.

--- src/main/java/dao/workerDao/constants/WorkerSelectorConstants.java
+++ src/main/java/dao/workerDao/constants/WorkerSelectorConstants.java
@@ -4,5 +4,5 @@ public class WorkerSelectorConstants extends SelectorConstantsAbstarct {
 
     public static final String OBJECT_TYPE = "Worker";
 
-    public static final String WROKER_SALARY = "Salary";
+    public static final String WORKER_SALARY = "Salary";
 }
